844afda3869ba8babc7fbca4565d4d81effaf95c
Zhenghao Zhu <zhenzhu@cse376u18.cs.stonybrook.edu>
2021-03-23 15:17:59 -0400

    Small change

--- tests/test1.sh
+++ tests/test1.sh
@@ -2,6 +2,7 @@
 
 echo "================================= Normal Malloc, Realloc, Normal Free, Approx Free Test ================================="
 
+mkdir output
 gcc -o tests/lkmalloc.o src/lkmalloc.c -c -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
 ar rcs tests/lkmalloc.a tests/lkmalloc.o
 gcc -c tests/lkmalloc_test_1.c -o tests/lkmalloc_test_1.o -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
